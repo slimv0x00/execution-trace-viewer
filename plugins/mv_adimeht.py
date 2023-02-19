5f1d05608873909268edfa14c204f3a7a01a4774
slimv0x00 <slimv0x00@gmail.com>
2023-02-19 21:32:55 +0900

    feat. add taint handle to some instruction

--- plugins/mv_adimeht.py
+++ plugins/mv_adimeht.py
@@ -3,18 +3,20 @@ from operator import itemgetter
 from core.api import Api
 import capstone
 
-class PluginMvAdimeht(IPlugin):
 
+class PluginMvAdimeht(IPlugin):
     md = None
     api = None
 
+    tainted = []
+
     def get_register_value_from_trace(self, trace, reg_name):
         _reg_index = {
-            'eax': 0, 'ebx': 1, 'ecx': 2, 'edx': 3, 'ebp': 4, 'esp': 5, 'esi': 6, 'edi': 7,
+            'eax': 0, 'ecx': 1, 'edx': 2, 'ebx': 3, 'esp': 4, 'ebp': 5, 'esi': 6, 'edi': 7,
             'eip': 8, 'eflags': 9,
-            'ax': 0, 'bx': 1, 'cx': 2, 'dx': 3, 'bp': 4, 'sp': 5, 'si': 6, 'di': 7,
-            'ah': 0, 'bh': 1, 'ch': 2, 'dh': 3,
-            'al': 0, 'bl': 1, 'cl': 2, 'dl': 3,
+            'ax': 0, 'cx': 1, 'dx': 2, 'bx': 3, 'sp': 4, 'bp': 5, 'si': 6, 'di': 7,
+            'ah': 0, 'ch': 1, 'dh': 2, 'bh': 3,
+            'al': 0, 'cl': 1, 'dl': 2, 'bl': 3,
         }
         return trace['regs'][_reg_index[reg_name]]
 
@@ -116,30 +118,162 @@ class PluginMvAdimeht(IPlugin):
 
         return _operands
 
-    def get_dst_src(self, trace, instruction):
+    def _run_taint(self, trace, instruction, dst, src):
+        _is_tainted = False
+        for _src in src:
+            if _src['name'] in self.tainted:
+                # self.api.print('[!] %s' % _src['name'])
+                _is_tainted = True
+                break
+        if _is_tainted:
+            for _dst in dst:
+                if _dst['name'] not in self.tainted:
+                    # self.api.print('[+] %s' % _dst['name'])
+                    self.tainted.append(_dst['name'])
+        else:
+            for _dst in dst:
+                if _dst['name'] in self.tainted:
+                    # self.api.print('[-] %s' % _dst['name'])
+                    self.tainted.remove(_dst['name'])
+
+    def get_dst_src(self, trace, instruction, operands):
         _dst = []
         _src = []
+        self.api.print(trace)
+        self.api.print(operands)
+        if len(instruction.groups) > 0:
+            for _g in instruction.groups:
+                if _g == capstone.x86.X86_GRP_CALL:
+                    _operand_value = self.get_register_value_from_trace(trace, 'esp') - 4
+                    _dst.append({
+                        'type': 'mem',
+                        'name': '[0x%x]' % _operand_value,
+                        'value': _operand_value,
+                        'formula': '[ esp - 4 ]'.split(' '),
+                    })
+                    if len(operands) == 0 or len(operands) > 1:
+                        return None, None
+                    _src.append(operands[0])
+                    return _dst, _src
+                elif _g == capstone.x86.X86_GRP_JUMP:
+                    if len(operands) == 0 or len(operands) > 1:
+                        return None, None
+                    _src.append(operands[0])
+                    return _dst, _src
+
+        if instruction.id == capstone.x86.X86_INS_PUSH:
+            _operand_value = self.get_register_value_from_trace(trace, 'esp') - 4
+            _dst.append({
+                'type': 'mem',
+                'name': '[0x%x]' % _operand_value,
+                'value': _operand_value,
+                'formula': '[ esp - 4 ]'.split(' '),
+            })
+            if len(operands) == 0 or len(operands) > 1:
+                return None, None
+            _src.append(operands[0])
+        elif instruction.id == capstone.x86.X86_INS_PUSHFD:
+            _operand_value = self.get_register_value_from_trace(trace, 'esp') - 4
+            _dst.append({
+                'type': 'mem',
+                'name': '[0x%x]' % _operand_value,
+                'value': _operand_value,
+                'formula': '[ esp - 4 ]'.split(' '),
+            })
+            _reg_name = 'eflags'
+            _src.append({
+                'type': 'reg',
+                'name': _reg_name,
+                'value': self.get_register_value_from_trace(trace, _reg_name),
+                'formula': None,
+            })
+        elif instruction.id == capstone.x86.X86_INS_POP:
+            _operand_value = self.get_register_value_from_trace(trace, 'esp')
+            _src.append({
+                'type': 'mem',
+                'name': '[0x%x]' % _operand_value,
+                'value': _operand_value,
+                'formula': '[ esp ]'.split(' '),
+            })
+            if len(operands) == 0 or len(operands) > 1:
+                return None, None
+            _dst.append(operands[0])
+        elif instruction.id in [capstone.x86.X86_INS_MOV, capstone.x86.X86_INS_ADD, capstone.x86.X86_INS_SUB,
+                                capstone.x86.X86_INS_AND, capstone.x86.X86_INS_OR]:
+            if len(operands) == 0 or len(operands) > 2:
+                return None, None
+            _dst.append(operands[0])
+            _src.append(operands[1])
+        elif instruction.id == capstone.x86.X86_INS_XOR:
+            if len(operands) != 2:
+                return None, None
+            if operands[0]['name'] == operands[1]['name']:
+                if operands[0]['name'] in self.tainted:
+                    self.tainted.remove(operands[0]['name'])
+                return [], []
+            _was_tainted_1 = operands[1]['name'] in self.tainted
+            if _was_tainted_1:
+                if operands[0]['name'] not in self.tainted:
+                    self.tainted.append(operands[0]['name'])
+        elif instruction.id == capstone.x86.X86_INS_XCHG:
+            if len(operands) != 2:
+                return None, None
+            _was_tainted_0 = operands[0]['name'] in self.tainted
+            _was_tainted_1 = operands[1]['name'] in self.tainted
+            if _was_tainted_0:
+                self.tainted.remove(operands[0]['name'])
+            if _was_tainted_1:
+                self.tainted.remove(operands[1]['name'])
+            if _was_tainted_0:
+                self.tainted.append(operands[1]['name'])
+            if _was_tainted_1:
+                self.tainted.append(operands[0]['name'])
+            return [], []
+        elif instruction.id == capstone.x86.X86_INS_CMPXCHG:
+            if len(operands) != 2:
+                return None, None
+            _was_tainted_1 = operands[1]['name'] in self.tainted
+            if _was_tainted_1 is False:
+                return [], []
+            for _mem in trace['mem']:
+                if _mem['access'] == 'WRITE' and _mem['addr'] == operands[0]['value']:
+                    if operands[0]['name'] in self.tainted:
+                        self.tainted.remove(operands[0]['name'])
+                        break
+            return [], []
+        elif instruction.id in [capstone.x86.X86_INS_INC, capstone.x86.X86_INS_DEC, capstone.x86.X86_INS_NOT,
+                                capstone.x86.X86_INS_TEST, capstone.x86.X86_INS_CMP, capstone.x86.X86_INS_SHL]:
+            return [], []
+        else:
+            self.api.print('[E] Instruction ID : %s (https://github.com/capstone-engine/capstone/blob/master/include/capstone/x86.h)' % instruction.id)
+            return None, None
         return _dst, _src
 
     def run_taint(self, trace):
         _instructions = self.md.disasm(bytes.fromhex(trace['opcodes']), trace['ip'])
         for _inst in _instructions:
             _operands = self.get_operands(trace, _inst)
-            _dst, _src = self.get_dst_src(trace, _inst)
+            _dst, _src = self.get_dst_src(trace, _inst, _operands)
             self.api.print('%d : 0x%x : %s : %s' % (trace['id'], trace['ip'], trace['disasm'], str(_operands)))
             self.api.print(' - src: %s' % _src)
             self.api.print(' - dst: %s' % _dst)
+            if _src is None:
+                return None
+            self._run_taint(trace, _inst, _dst, _src)
+            trace['comment'] = str(self.tainted)
+        return trace
 
     def execute(self, api: Api):
+        self.tainted = []
         self.api = api
         self.md = capstone.Cs(capstone.CS_ARCH_X86, capstone.CS_MODE_32)
         self.md.detail = True
         _input_dlg_data = [
             {'label': 'Trace boundary begin', 'data': '0x0'},
             {'label': 'Trace boundary end', 'data': '0x70000000'},
-            {'label': 'Target index(#)', 'data': 2},
-            {'label': 'Target operand', 'data': 'edx'},
-            {'label': 'TTL (no limit, -1)', 'data': 100},
+            {'label': 'Target index(#)', 'data': 0},
+            {'label': 'Target operand', 'data': 'eax'},
+            {'label': 'TTL (no limit, -1)', 'data': 1000},
         ]
         _options = self.api.get_values_from_user("Filter by memory address", _input_dlg_data)
         if not _options:
@@ -167,19 +301,23 @@ class PluginMvAdimeht(IPlugin):
             #   'mem': [{'access': 'WRITE', 'addr': 20472, 'value': 2852906762}],
             #   'regchanges': 'ebp: 0x4ff8 '
             # }
+            _index = _trace['id']
             if _ttl >= 0:
-                if _trace['id'] > _ttl:
+                if _index > _ttl:
                     break
             _eip = _trace['ip']
             if _eip < _trace_boundary_begin or _eip >= _trace_boundary_end:
                 continue
+            if _index == _target_index:
+                self.tainted.append(_target_operand)
 
-            self.run_taint(_trace)
-
-            _traces_to_show.append(_trace.copy())
+            _new_trace = self.run_taint(_trace)
+            if _new_trace is None:
+                _traces_to_show.append(_trace.copy())
+                break
+            _traces_to_show.append(_new_trace.copy())
 
         if len(_traces_to_show) > 0:
             print(f"Length of filtered trace: {len(_traces_to_show)}")
             self.api.set_filtered_trace(_traces_to_show)
             self.api.show_filtered_trace()
-
