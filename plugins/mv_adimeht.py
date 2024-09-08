11ecfb7d7fa13ad3eb8c284a0acc8ad30b21a98d
slimv <slimv0x00@gmail.com>
2024-09-08 17:27:38 +0900

    feat. recognizing VBR and arguments automatically without requiring any memory address information input

--- plugins/mv_adimeht.py
+++ plugins/mv_adimeht.py
@@ -29,8 +29,8 @@ class PluginMvAdimeht(IPlugin):
     taint_logging_on_adding_and_removing_tainted_operand: bool = True
     taint_logging_detail_of_tainted_operand_on_adding: bool = False
 
-    def get_registers_as_tainted_operand_list(self, trace) -> list[TraceAdimehtOperandForX64DbgTrace]:
-        self.context.set_context_by_x64dbg_trace(trace)
+    def get_registers_as_tainted_operand_list(self, x64dbg_trace) -> list[TraceAdimehtOperandForX64DbgTrace]:
+        self.context.set_context_by_x64dbg_trace(x64dbg_trace)
         _result: list[TraceAdimehtOperandForX64DbgTrace] = []
 
         # register EAX
@@ -64,140 +64,105 @@ class PluginMvAdimeht(IPlugin):
 
         return _result
 
-    def get_preset_of_tainted_operands_for_sample_1(self, trace):
-        _result = self.get_registers_as_tainted_operand_list(trace)
-        # argument 1
-        _arg_1 = TraceAdimehtOperandForX64DbgTrace(self.context, None)
-        _mem_addr = 0xd3f9f4
-        _arg_1.force_set_adimeht_operand(
-            'mem',
-            '[0x%08x]' % _mem_addr,
-            _mem_addr,
-            ('[ 0x%08x ]' % _mem_addr).split(' '),
-            ['arg_1'],
-            'IMM',
-        )
-        _result.append(_arg_1)
-        # argument 2
-        _arg_2 = TraceAdimehtOperandForX64DbgTrace(self.context, None)
-        _mem_addr = 0xd3f9f8
-        _arg_2.force_set_adimeht_operand(
-            'mem',
-            '[0x%08x]' % _mem_addr,
-            _mem_addr,
-            ('[ 0x%08x ]' % _mem_addr).split(' '),
-            ['arg_2'],
-            'IMM',
-        )
-        _result.append(_arg_2)
-        return _result
+    def recognize_arg_and_vbr(
+            self,
+            x64dbg_traces,
+            address_boundary_to_trace_begin,
+            address_boundary_to_trace_end,
+            target_index,
+            vbr,
+            number_of_arg,
+            int_size=4,
+    ):
+        _vbr_canditates = {}
+        _args = []
+        for _x64dbg_trace in x64dbg_traces:
+            try:
+                # _trace
+                # {
+                #   'id': 0,
+                #   'ip': 4242012,
+                #   'disasm': 'push 0xaa0be70a',
+                #   'comment': 'push encrypted vm_eip',
+                #   'regs': [3806, 309, 326, 292, 0, 20476, 360, 377, 4242012, 0],
+                #   'opcodes': '680ae70baa',
+                #   'mem': [{'access': 'WRITE', 'addr': 20472, 'value': 2852906762}],
+                #   'regchanges': 'ebp: 0x4ff8 '
+                # }
+                _index = _x64dbg_trace['id']
+                _eip = _x64dbg_trace['ip']
+                # skip tracing when EIP is outside the boundary to trace
+                if _eip < address_boundary_to_trace_begin or _eip >= address_boundary_to_trace_end:
+                    continue
+                self.context.set_context_by_x64dbg_trace(_x64dbg_trace)
+                if _index == target_index:
+                    _esp = self.context.get_register_value('esp')
+                    for _i_arg in range(number_of_arg):
+                        _args.append({
+                            'arg_id': 'arg_%d' % (_i_arg + 1),
+                            'arg_addr': _esp + (_i_arg * int_size),
+                        })
+                _ebp = self.context.get_register_value('ebp')
+                if _ebp not in _vbr_canditates:
+                    _vbr_canditates[_ebp] = 0
+                _vbr_canditates[_ebp] += 1
+            except Exception as e:
+                print(traceback.format_exc())
+                print(e)
+                print(_x64dbg_trace)
+                break
+        _vbr = vbr
+        _sorted_vbr_candidates = sorted(_vbr_canditates, key=lambda x: _vbr_canditates[x], reverse=True)
+        if _vbr < 0:
+            if len(_sorted_vbr_candidates) <= 0:
+                print('[E] Cannot find any candidates for vbr')
+            _vbr = _sorted_vbr_candidates[0]
+        self.api.print('[+] VBR : 0x%x' % _vbr)
+        _len_vbr_candidates = 3 if len(_sorted_vbr_candidates) > 3 else len(_sorted_vbr_candidates)
+        for _i_vbr_canditate in range(_len_vbr_candidates):
+            self.api.print(' - VBR canditate %d : 0x%x (%d-hit)' % (
+                _i_vbr_canditate + 1,
+                _sorted_vbr_candidates[_i_vbr_canditate],
+                _vbr_canditates[_sorted_vbr_candidates[_i_vbr_canditate]]
+            ))
+        if len(_args) > 0:
+            self.api.print('[+] Arguments')
+            for _arg in _args:
+                self.api.print(' - %s : 0x%x' % (_arg['arg_id'], _arg['arg_addr']))
+        # {
+        #    'args': [
+        #        { 'arg_id': 'arg_1', 'arg_addr': 0x19ff00 },
+        #        { 'arg_id': 'arg_2', 'arg_addr': 0x19ff04 },
+        #        ...,
+        #    ],
+        #    'vbr': 0xc5fb90,
+        #    'vbr_candidates': {
+        #        0xc5fb90: 1000,
+        #        0xc5ee80: 10,
+        #        ...,
+        #    }
+        # }
+        return {
+            'args': _args,
+            'vbr': _vbr,
+            'vbr_candidates': _vbr_canditates,
+        }
 
-    def get_preset_of_tainted_operands_for_sample_2(self, trace):
-        _result = self.get_registers_as_tainted_operand_list(trace)
-        # argument 1
-        _arg_1 = TraceAdimehtOperandForX64DbgTrace(self.context, None)
-        _mem_addr = 0xcff9c8
-        _arg_1.force_set_adimeht_operand(
-            'mem',
-            '[0x%08x]' % _mem_addr,
-            _mem_addr,
-            ('[ 0x%08x ]' % _mem_addr).split(' '),
-            ['arg_1'],
-            'IMM',
-        )
-        _result.append(_arg_1)
-        # argument 2
-        _arg_2 = TraceAdimehtOperandForX64DbgTrace(self.context, None)
-        _mem_addr = 0xcff9cc
-        _arg_2.force_set_adimeht_operand(
-            'mem',
-            '[0x%08x]' % _mem_addr,
-            _mem_addr,
-            ('[ 0x%08x ]' % _mem_addr).split(' '),
-            ['arg_2'],
-            'IMM',
-        )
-        _result.append(_arg_2)
-        # argument 3
-        _arg_3 = TraceAdimehtOperandForX64DbgTrace(self.context, None)
-        _mem_addr = 0xcff9d0
-        _arg_3.force_set_adimeht_operand(
-            'mem',
-            '[0x%08x]' % _mem_addr,
-            _mem_addr,
-            ('[ 0x%08x ]' % _mem_addr).split(' '),
-            ['arg_3'],
-            'IMM',
-        )
-        _result.append(_arg_3)
-        return _result
-
-    def get_preset_of_tainted_operands_for_sample_3(self, trace):
-        _result = self.get_registers_as_tainted_operand_list(trace)
-        # argument 1
-        _arg_1 = TraceAdimehtOperandForX64DbgTrace(self.context, None)
-        _mem_addr = 0x19ff0c
-        _arg_1.force_set_adimeht_operand(
-            'mem',
-            '[0x%08x]' % _mem_addr,
-            _mem_addr,
-            ('[ 0x%08x ]' % _mem_addr).split(' '),
-            ['arg_1'],
-            'IMM',
-        )
-        _result.append(_arg_1)
-        # argument 2
-        _arg_2 = TraceAdimehtOperandForX64DbgTrace(self.context, None)
-        _mem_addr = 0x19ff10
-        _arg_2.force_set_adimeht_operand(
-            'mem',
-            '[0x%08x]' % _mem_addr,
-            _mem_addr,
-            ('[ 0x%08x ]' % _mem_addr).split(' '),
-            ['arg_2'],
-            'IMM',
-        )
-        _result.append(_arg_2)
-        return _result
-
-    def get_preset_of_tainted_operands_for_sample_4(self, trace):
-        _result = self.get_registers_as_tainted_operand_list(trace)
-        # argument 1
-        _arg_1 = TraceAdimehtOperandForX64DbgTrace(self.context, None)
-        _mem_addr = 0x19ff04
-        _arg_1.force_set_adimeht_operand(
-            'mem',
-            '[0x%08x]' % _mem_addr,
-            _mem_addr,
-            ('[ 0x%08x ]' % _mem_addr).split(' '),
-            ['arg_1'],
-            'IMM',
-        )
-        _result.append(_arg_1)
-        # argument 2
-        _arg_2 = TraceAdimehtOperandForX64DbgTrace(self.context, None)
-        _mem_addr = 0x19ff08
-        _arg_2.force_set_adimeht_operand(
-            'mem',
-            '[0x%08x]' % _mem_addr,
-            _mem_addr,
-            ('[ 0x%08x ]' % _mem_addr).split(' '),
-            ['arg_2'],
-            'IMM',
-        )
-        _result.append(_arg_2)
-        # argument 3
-        _arg_3 = TraceAdimehtOperandForX64DbgTrace(self.context, None)
-        _mem_addr = 0x19ff0c
-        _arg_3.force_set_adimeht_operand(
-            'mem',
-            '[0x%08x]' % _mem_addr,
-            _mem_addr,
-            ('[ 0x%08x ]' % _mem_addr).split(' '),
-            ['arg_3'],
-            'IMM',
-        )
-        _result.append(_arg_3)
+    def get_initial_tainted_operands(self, x64dbg_trace, args):
+        _result = self.get_registers_as_tainted_operand_list(x64dbg_trace)
+        for _arg in args:
+            _arg_id = _arg['arg_id']
+            _arg_addr = _arg['arg_addr']
+            _trace_adimeht_arg = TraceAdimehtOperandForX64DbgTrace(self.context, None)
+            _trace_adimeht_arg.force_set_adimeht_operand(
+                'mem',
+                '[0x%08x]' % _arg_addr,
+                _arg_addr,
+                ('[ 0x%08x ]' % _arg_addr).split(' '),
+                [_arg_id],
+                'IMM',
+            )
+            _result.append(_trace_adimeht_arg)
         return _result
 
     def execute(self, api: Api):
@@ -205,14 +170,12 @@ class PluginMvAdimeht(IPlugin):
         self.capstone_bridge = capstone.Cs(capstone.CS_ARCH_X86, capstone.CS_MODE_32)
         self.capstone_bridge.detail = True
         self.context = TraceContext(capstone_bridge=self.capstone_bridge)
-        _preset_id = 1
         _input_dlg_data = [
             {'label': 'Trace boundary begin', 'data': '0x0'},
             {'label': 'Trace boundary end', 'data': '0x70000000'},
-            {'label': 'Target index(#)', 'data': 0},
-            {'label': 'Target operand (reg or preset)', 'data': 'preset%d' % _preset_id},
-            {'label': 'Target desc (when it\'s reg)', 'data': 'preset%d' % _preset_id},
-            {'label': 'VBR (Virtual Base Register)', 'data': 'preset%d' % _preset_id},
+            {'label': 'Target index (where you want to pick arg)', 'data': 0},
+            {'label': 'VBR (Virtual Base Register, -1 when you have no idea)', 'data': -1},
+            {'label': 'Number of argument', 'data': 2},
             {'label': 'TTL (no limit, -1)', 'data': -1},
         ]
         _options = self.api.get_values_from_user("Filter by memory address", _input_dlg_data)
@@ -221,28 +184,24 @@ class PluginMvAdimeht(IPlugin):
         _str_address_boundary_to_trace_begin,\
             _str_address_boundary_to_trace_end,\
             _target_index,\
-            _target_operand,\
-            _target_description,\
-            _str_vbr, \
+            _vbr, \
+            _number_of_arg, \
             _ttl = _options
         _address_boundary_to_trace_begin = int(_str_address_boundary_to_trace_begin, 16)
         _address_boundary_to_trace_end = int(_str_address_boundary_to_trace_end, 16)
 
-        _vbr: int | None = None
-        if _str_vbr == 'preset1':
-            # sample1_vm_addTwo
-            _vbr = 0x55568a
-        elif _str_vbr == 'preset2':
-            # sample2_vm_addTwo
-            _vbr = 0x3d7628
-        elif _str_vbr == 'preset3':
-            # sample1_vm_addTwo_3.1.8
-            _vbr = 0x462f0c
-        elif _str_vbr == 'preset4':
-            # sample2_vm_addTwo_3.1.8
-            _vbr = 0x4543ea
-        # else:
-        #     _vbr = int(_str_vbr, 16)
+        self.api.print('[+] Recognizing %d-arguments and VBR' % _number_of_arg)
+        _x64dbg_traces = self.api.get_full_trace()
+        _arg_and_vbr = self.recognize_arg_and_vbr(
+            _x64dbg_traces,
+            _address_boundary_to_trace_begin,
+            _address_boundary_to_trace_end,
+            _target_index,
+            _vbr,
+            _number_of_arg,
+        )
+        _vbr = _arg_and_vbr['vbr']
+        _args = _arg_and_vbr['args']
 
         self.adimehtModule = TraceAdimeht(
             api,
@@ -263,15 +222,10 @@ class PluginMvAdimeht(IPlugin):
             logging_on_adding_and_removing_tainted_operand=self.taint_logging_on_adding_and_removing_tainted_operand,
             logging_detail_of_tainted_operand_on_adding=self.taint_logging_detail_of_tainted_operand_on_adding,
         )
-
         self.api.print('[+] Run taint analysis')
         self.api.print(' - Address boundary to trace : 0x%08x ~ 0x%08x'
                        % (_address_boundary_to_trace_begin, _address_boundary_to_trace_end))
-        self.api.print(' - Initial target to trace : %s at %d' % (_target_operand, _target_index))
-
-        _x64dbg_traces = self.api.get_full_trace()
         _traces_to_show = []
-
         for _x64dbg_trace in _x64dbg_traces:
             try:
                 # _trace
@@ -295,17 +249,9 @@ class PluginMvAdimeht(IPlugin):
                     continue
 
                 if _index == _target_index:
-                    _preset = []
-                    if _target_operand == 'preset1':
-                        _preset = self.get_preset_of_tainted_operands_for_sample_1(_x64dbg_trace)
-                    elif _target_operand == 'preset2':
-                        _preset = self.get_preset_of_tainted_operands_for_sample_2(_x64dbg_trace)
-                    elif _target_operand == 'preset3':
-                        _preset = self.get_preset_of_tainted_operands_for_sample_3(_x64dbg_trace)
-                    elif _target_operand == 'preset4':
-                        _preset = self.get_preset_of_tainted_operands_for_sample_4(_x64dbg_trace)
-                    self.adimehtModule.set_tainted_operands(_preset[:])
-                    self.taintModule.set_tainted_operands(_preset[:])
+                    _initial_tainted_operands = self.get_initial_tainted_operands(_x64dbg_trace, _args)
+                    self.adimehtModule.set_tainted_operands(_initial_tainted_operands[:])
+                    self.taintModule.set_tainted_operands(_initial_tainted_operands[:])
                     self.api.print('[+] Initial tainted operands are set : ')
                     [self.api.print(str(_op)) for _op in self.adimehtModule.tainted_operands]
 
